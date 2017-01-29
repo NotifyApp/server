54e0ba1e4770cc2d85f2a1de678ac0d916c2dd5f
ItsJimi <jimi.maiquez@gmail.com>
2017-01-29 22:39:06 +0100

    Add POST => /notify

--- index.js
+++ index.js
@@ -4,20 +4,30 @@ const WebSocketServer = require('ws').Server
 const wss = new WebSocketServer({ server: server })
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 5000
- 
-app.use((req, res) => {
-  res.send({ msg: "hello" })
-})
- 
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log('received: %s', message)
-  })
 
+app.use(bodyParser.json())
+
+wss.on('connection', (ws) => {
   ws.send('something')
 })
- 
+
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Notify API',
+    version: "1.0.0"
+  })
+})
+
+app.post('/notify', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Sent!'
+  })
+})
+
 server.on('request', app);
 server.listen(port, () => {
   console.log('Listening on ' + port)
